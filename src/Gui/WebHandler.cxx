fa8caf693c4e8aebefcc8dce7ebdd656b38c7fad
bcpearce <admin@bcpearce.com>
2025-06-30 00:23:57 -0400

    add packing of web files

--- src/Gui/WebHandler.cxx
+++ src/Gui/WebHandler.cxx
@@ -1,7 +1,6 @@
 #include "WindowsWrapper.h"
 
 #include "Gui/WebHandler.h"
-#include "index.h"
 
 #include <filesystem>
 #include <iostream>
@@ -55,7 +54,9 @@ void EventHandler(mg_connection *c, int ev, void *ev_data) {
       c->data[0] = 'M';
       mg_printf(c, "%s", mjpegHeaders);
     } else {
-      mg_http_reply(c, 200, "", "%s", web_public::indexHtml);
+      // mg_http_reply(c, 200, "", "%s", web_public::indexHtml);
+      mg_http_serve_opts opts = {.root_dir = "/public", .fs = &mg_fs_packed};
+      mg_http_serve_dir(c, hm, &opts);
     }
   } break;
   }
