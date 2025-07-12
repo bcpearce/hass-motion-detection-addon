46959e4a070b1e7ad74ada4b7538acfe49c485d8
bcpearce <admin@bcpearce.com>
2025-07-11 22:34:47 -0400

    fix program options

--- src/Util/ProgramOptions.cxx
+++ src/Util/ProgramOptions.cxx
@@ -17,53 +17,71 @@ namespace po = boost::program_options;
 
 namespace util {
 
-ProgramOptions::ProgramOptions(int argc, const char **argv, bool loadDotEnv) {
+ProgramOptions::ProgramOptions(int argc, const char **argv) {
   po::options_description desc("Allowed options");
 
-  desc.add_options()("help", "show help message");
-  desc.add_options()("version", "show version");
-  desc.add_options()("source-url", po::value<std::string>()->required(),
-                     "target video source URL");
-  desc.add_options()("source-username",
-                     po::value<std::string>()->default_value(""),
-                     "target video source username for basic or digest auth");
-  desc.add_options()("source-password",
-                     po::value<std::string>()->default_value(""),
-                     "target video source password for basic or digest auth");
-  desc.add_options()("source-token",
-                     po::value<std::string>()->default_value(""),
-                     "target video source token authentication");
-
-  desc.add_options()("hass-url", po::value<std::string>()->default_value(""),
-                     "Home Assistant URL to send detector updates");
-  desc.add_options()("hass-entity-id",
-                     po::value<std::string>()->default_value(""),
-                     "Home Assistant entity ID to update");
-  desc.add_options()("hass-friendly-name",
-                     po::value<std::string>()->default_value(""),
-                     "Home Assistant entity Friendly Name (to be sent if the "
-                     "entity ID does not already exist)");
-  desc.add_options()("hass-token", po::value<std::string>()->default_value(""),
-                     "Home Assistant long-lived access token for API auth");
-
-  desc.add_options()("web-ui-host",
-                     po::value<std::string>()->default_value("0.0.0.0"),
-                     "host to bind the web UI to, defaults to localhost");
-  desc.add_options()("web-ui-port", po::value<int>()->default_value(32834),
-                     "port to bind the web UI to, defaults to 32834");
-
-  desc.add_options()("detection-size", po::value<std::string>(),
-                     "minimum size of a motion detection 'blob' that registers "
-                     "as a region of interest\n"
-                     "can be registered as an integer pixel count e.g. '500' "
-                     "or a percentage of the total pixels e.g. '5%'");
-  desc.add_options()(
-      "detection-debounce", po::value<int>(),
-      "minimum time between state changes in detection events (i.e. going from "
-      "no motion to motion detected) in seconds");
+  desc.add_options()
+      /*
+       * Meta Options
+       */
+      ("help", "show help message")
+      /**/
+      ("version", "show version")
+      /*
+       * Source RTSP Feed Options
+       */
+      ("source-url", po::value<std::string>()->required(),
+       "target video source URL")
+      /**/
+      ("source-username", po::value<std::string>()->default_value(""),
+       "target video source username for basic or digest auth")
+      /**/
+      ("source-password", po::value<std::string>()->default_value(""),
+       "target video source password for basic or digest auth")
+      /**/
+      ("source-token", po::value<std::string>()->default_value(""),
+       "target video source token authentication")
+      /*
+       * Home Assistant Handler Options
+       */
+      ("hass-url", po::value<std::string>()->default_value(""),
+       "Home Assistant URL to send detector updates")
+      /**/
+      ("hass-entity-id", po::value<std::string>()->default_value(""),
+       "Home Assistant entity ID to update")
+      /**/
+      ("hass-friendly-name", po::value<std::string>()->default_value(""),
+       "Home Assistant entity Friendly Name (to be sent if the entity ID does "
+       "not already exist)")
+      /**/
+      ("hass-token", po::value<std::string>()->default_value(""),
+       "Home Assistant long-lived access token for API auth")
+      /*
+       * Web User-Interface Options
+       */
+      ("web-ui-host", po::value<std::string>()->default_value("0.0.0.0"),
+       "host to bind the web UI to, defaults to localhost, set to '' to "
+       "disable the Web GUI")
+      /**/
+      ("web-ui-port", po::value<int>()->default_value(32834),
+       "port to bind the web UI to, defaults to 32834, set to <= 0 to disable "
+       "the Web GUI")
+      /*
+       * Detection Options
+       */
+      ("detection-size", po::value<std::string>(),
+       "minimum size of a motion detection 'blob' that registers as a region "
+       "of interest\n"
+       "can be registered as an integer pixel count e.g. '500' or a percentage "
+       "of the total pixels e.g. '5%'")
+      /**/
+      ("detection-debounce", po::value<int>(),
+       "minimum time between state changes in detection events (i.e. going "
+       "from no motion to motion detected) in seconds")
+      /**/;
 
   po::positional_options_description posOpts;
-  posOpts.add("source-url", -1);
+  posOpts.add("source-url", 1);
 
   po::variables_map vm;
 
@@ -75,43 +93,21 @@ ProgramOptions::ProgramOptions(int argc, const char **argv, bool loadDotEnv) {
 
   const auto parsedEnv =
       po::parse_environment(desc, [](std::string_view envVar) {
-        if (envVar == "USERNAME"sv) {
-          return "source-username"s;
-        }
-        if (envVar == "PASSWORD"sv) {
-          return "source-password"s;
-        }
-        if (envVar == "TOKEN"sv) {
-          return "source-token"s;
-        }
-        if (envVar == "URL"sv) {
-          return "source-url"s;
-        }
-        if (envVar == "HASS_URL"sv) {
-          return "hass-url"s;
-        }
-        if (envVar == "HASS_ENTITY_ID"sv) {
-          return "hass-entity-id"s;
-        }
-        if (envVar == "HASS_FRIENDLY_NAME"sv) {
-          return "hass-friendly-name"s;
-        }
-        if (envVar == "HASS_TOKEN"sv) {
-          return "hass-token"s;
-        }
-        if (envVar == "WEB_UI_HOST"sv) {
-          return "web-ui-host"s;
-        }
-        if (envVar == "WEB_UI_PORT"sv) {
-          return "web-ui-port"s;
-        }
-        if (envVar == "DETECTION_SIZE"sv) {
-          return "detection-size"s;
-        }
-        if (envVar == "DETECTION_DEBOUNCE"sv) {
-          return "detection-debounce"s;
-        }
-        return ""s;
+        static const std::map<std::string, std::string, std::less<>>
+            envVarToProgOpts{{"USERNAME"s, "source-username"s},
+                             {"PASSWORD"s, "source-password"s},
+                             {"TOKEN"s, "source-token"s},
+                             {"URL"s, "source-url"s},
+                             {"HASS_URL"s, "hass-url"s},
+                             {"HASS_ENTITY_ID"s, "hass-entity-id"s},
+                             {"HASS_TOKEN"s, "hass-token"},
+                             {"HASS_FRIENDLY_NAME"s, "hass-friendly-name"s},
+                             {"WEB_UI_HOST"s, "web-ui-host"s},
+                             {"WEB_UI_PORT"s, "web-ui-port"s},
+                             {"DETECTION_SIZE"s, "detection-size"s},
+                             {"DETECTION_DEBOUNCE"s, "detection-debounce"s}};
+        const auto it = envVarToProgOpts.find(envVar);
+        return it != envVarToProgOpts.end() ? it->second : ""s;
       });
   po::store(parsedEnv, vm);
 
