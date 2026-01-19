b78b046bb3909ca9124ff647e4bd52b7fa14fb1b
Benjamin Pearce <admin@bcpearce.com>
2026-01-19 01:55:55 -0500

    Removed optional server port

--- tests/ServerEnv.h
+++ tests/ServerEnv.h
@@ -4,12 +4,6 @@
 
 class ServerEnvironment : public ::testing::Environment {
 public:
-  void SetUp() override {
-    if (const auto *port = std::getenv("SIM_SERVER_PORT")) {
-      SimServer::Start(std::stoi(port));
-    } else {
-      SimServer::Start(SIM_SERVER_PORT);
-    }
-  }
+  void SetUp() override { SimServer::Start(SIM_SERVER_PORT); }
   void TearDown() override { SimServer::Stop(); }
 };
