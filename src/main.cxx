c2d89ddaff4aef3e271ac1ceed5ed970cc5fa3c3
Benjamin Pearce <admin@bcpearce.com>
2026-01-26 22:33:17 -0500

    Moved end-to-end test into C++

--- src/main.cxx
+++ src/main.cxx
@@ -50,25 +50,9 @@ struct SourceAndHandlers {
 
 static ExitSignalHandler exitSignalHandler;
 
-#ifdef _WIN32
-BOOL WINAPI SignalHandlerWrapper(DWORD dwCtrlType) {
-  switch (dwCtrlType) {
-  case CTRL_C_EVENT:
-  case CTRL_BREAK_EVENT:
-    exitSignalHandler(SIGINT);
-    return TRUE;
-  case CTRL_CLOSE_EVENT:
-    exitSignalHandler(SIGTERM);
-    return TRUE;
-  default:
-    return FALSE;
-  }
-}
-#elifdef __linux__
 void SignalHandlerWrapper(int signal) {
   exitSignalHandler(signal); // Call the functor's operator()
 }
-#endif
 
 } // namespace
 
@@ -202,12 +186,8 @@ void App(const util::ProgramOptions &opts) {
     }
   }
 
-#ifdef _WIN32
-  SetConsoleCtrlHandler(SignalHandlerWrapper, TRUE);
-#elifdef __linux__
   std::signal(SIGINT, SignalHandlerWrapper);
   std::signal(SIGTERM, SignalHandlerWrapper);
-#endif
   for (auto pSource :
        sources | std::views::transform(&SourceAndHandlers::pSource)) {
     if (pSource) {
