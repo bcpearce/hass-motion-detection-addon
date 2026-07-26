3b29b92662bda97905ea6ad4979c6e58f28881c2
Benjamin Pearce <bcpearce@users.noreply.github.com>
2026-07-26 19:01:30 -0400

    Update Addon to use multi-feed (#31)

--- motion_detection_addon/run.sh
+++ motion_detection_addon/run.sh
@@ -7,27 +7,48 @@ declare -a args=("$@")
 args+=(--hass-url 'http://supervisor')
 args+=(--hass-token "$SUPERVISOR_TOKEN")
 
-# required args passed in from the config
-args+=(--source-url "$(bashio::config 'url')")
-args+=(--hass-entity-id "$(bashio::config 'entity_id')")
+read_config_value() {
+  local key="$1"
+  local value
+  value="$(bashio::config "$key")"
+  if [[ "$value" == "null" ]]; then
+    echo ""
+  else
+    echo "$value"
+  fi
+}
 
-# required args if authentication with the video source is needed
-if [[ $(bashio::config 'password') != null ]]; then
-  args+=(--source-password "$(bashio::config 'password')")
-fi
-if [[ $(bashio::config 'username') != null ]]; then
-  args+=(--source-username "$(bashio::config 'username')")
-fi
-if [[ $(bashio::config 'token') != null ]]; then
-  args+=(--source-token "$(bashio::config 'token')")
-fi
+build_feed_config() {
+  local feeds_json="$1"
+  local legacy_url="$2"
+  local legacy_entity="$3"
+  local legacy_username="$4"
+  local legacy_password="$5"
+  local legacy_token="$6"
+  local legacy_debounce="$7"
+  local legacy_size="$8"
+  local feed_config_script="${FEED_CONFIG_SCRIPT:-/build_feed_config.py}"
 
-if [[ $(bashio::config 'detection_debounce_seconds') != null ]]; then
- args+=(--detection-debounce "$(bashio::config 'detection_debounce_seconds')")
-fi
+  if [[ ! -f "$feed_config_script" ]]; then
+    feed_config_script="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/build_feed_config.py"
+  fi
 
-if [[ $(bashio::config 'detection_size') != null ]]; then
- args+=(--detection-size "$(bashio::config 'detection_size')")
+  python3 "$feed_config_script" "$feeds_json" "$legacy_url" "$legacy_entity" "$legacy_username" \
+    "$legacy_password" "$legacy_token" "$legacy_debounce" "$legacy_size"
+}
+
+feeds_config="$(read_config_value 'feeds')"
+legacy_url="$(read_config_value 'url')"
+legacy_entity="$(read_config_value 'entity_id')"
+legacy_username="$(read_config_value 'username')"
+legacy_password="$(read_config_value 'password')"
+legacy_token="$(read_config_value 'token')"
+legacy_debounce="$(read_config_value 'detection_debounce_seconds')"
+legacy_size="$(read_config_value 'detection_size')"
+
+feed_config_json="$(build_feed_config "$feeds_config" "$legacy_url" "$legacy_entity" "$legacy_username" "$legacy_password" "$legacy_token" "$legacy_debounce" "$legacy_size")"
+if [[ -n "$feed_config_json" && "$feed_config_json" != '{}' ]]; then
+  args+=(--source-config-raw "$feed_config_json")
 fi
 
 echo "${args[@]}"
